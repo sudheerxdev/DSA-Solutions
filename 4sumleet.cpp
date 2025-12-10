a67df6df50ad458a870debd7e3df0ffbefca2430
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-12-10 17:22:58 +0530

    Modify 4sumleet.cpp for improved readability

--- 4sumleet.cpp
+++ 4sumleet.cpp
@@ -7,6 +7,7 @@ public:
         vector<vector<int>> output;
         for(int i=0; i<n-3; i++){
             for(int j=i+1; j<n-2; j++){
+                
                 for(int k=j+1; k<n-1; k++){
                     for(int l=k+1; l<n; l++){
                         
