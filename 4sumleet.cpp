e5928f3a472e339e97023e7e67a874180b6e7d19
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-12-10 17:23:09 +0530

    Fix formatting issues in 4sumleet.cpp

--- 4sumleet.cpp
+++ 4sumleet.cpp
@@ -5,6 +5,7 @@ public:
         sort(nums.begin(), nums.end());
         set<vector<int>> set;
         vector<vector<int>> output;
+        
         for(int i=0; i<n-3; i++){
             for(int j=i+1; j<n-2; j++){
                 
