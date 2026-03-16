be25947365ea97b6c7f36ceb86e5383d73b38fd3
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2026-03-16 18:57:22 +0530

    Add files via upload

---
+++ IDE_Code/CppCPractice/rangeofdatatype.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <climits>
+using namespace std;
+int add(int a , int b){
+    return a+b;
+}
+int main(){
+    // int a  , b ;
+    // cin >> a  >> b ;
+    // cout << add(a , b); 
+     
+
+    cout << INT_MAX ; 
+    // cout << INT_MAX + 1;
+    int i = 2147483647;
+    cout << i+ 1;
+}
+ // max valye = 2^31 - 1 
+ // min value = 2^31 
