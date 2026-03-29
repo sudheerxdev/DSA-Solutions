7babe9c15e1134dd9afc38c439d250823c31c5be
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2026-03-29 23:05:58 +0530

    Fix formatting and improve readability in solution.cpp

--- LeetCode-Solutions/Hard/MinimumOneBitOperationsToMakeIntegersZero/solution.cpp
+++ LeetCode-Solutions/Hard/MinimumOneBitOperationsToMakeIntegersZero/solution.cpp
@@ -3,7 +3,6 @@ public:
     int minimumOneBitOperations(int n) { 
 
 
-
         return (n==0)? 0: ((1<<(32-countl_zero(unsigned(n))))-1-minimumOneBitOperations(n^bit_floor(unsigned(n))));
     }
 };
