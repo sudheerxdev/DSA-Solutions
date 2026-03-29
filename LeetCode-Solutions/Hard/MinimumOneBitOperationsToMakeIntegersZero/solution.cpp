823b955bd4aac00a59ed1d45dff2a37add7c0ea6
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2026-03-29 23:04:53 +0530

    Update solution.cpp

--- LeetCode-Solutions/Hard/MinimumOneBitOperationsToMakeIntegersZero/solution.cpp
+++ LeetCode-Solutions/Hard/MinimumOneBitOperationsToMakeIntegersZero/solution.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int minimumOneBitOperations(int n) {
+
+
+
+        
         return (n==0)? 0: ((1<<(32-countl_zero(unsigned(n))))-1-minimumOneBitOperations(n^bit_floor(unsigned(n))));
     }
 };
