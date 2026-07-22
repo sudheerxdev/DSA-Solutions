f674bf4c132f5b3924896119001c157f9af3c4fa
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2026-07-22 21:49:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum Deletions to Make Sorted/minimum-deletions-to-make-sorted.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minDeletions(vector<int>& arr) {
+        vector<int> lis;
+
+        for (int x : arr) {
+            auto it = lower_bound(lis.begin(), lis.end(), x);
+
+            if (it == lis.end())
+                lis.push_back(x);
+            else
+                *it = x;
+        }
+
+        return arr.size() - lis.size();
+    }
+};
+//GFG POTD solution for 22 July
